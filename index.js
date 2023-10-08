09b2497284e7c722f55ccdcb5890467a64ec8102
Mrkishansharma <kishansharma6377@gmail.com>
2023-10-08 16:39:16 +0530

    server setup,connect to database and  model created successfully.

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
+const { connection } = require("./config/db");
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.status(200).send("Welcome To TaskManagement API.")
+})
+
+
+app.all('*', (req, res) => {
+    return res.status(404).send({
+        "error": "404! Route Not Found"
+    })
+})
+
+app.listen(process.env.port, async () => {
+    try {
+        await connection;
+        console.log("Connected to DB");
+    } catch (error) {
+        console.error(error);
+    }
+    console.log(`Server is running on port ${process.env.port}`);
+})
+
+
+
